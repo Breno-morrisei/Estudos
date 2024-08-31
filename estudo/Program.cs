b4e80bace479c71de6fdd8e128c206740c51de40
breno <breno@DESKTOP-C1CL0FN>
2024-08-31 15:26:46 -0300

    Adicionar arquivos de projeto.

---
+++ estudo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.Write("Digite seu E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Confirme seu E-mail:");
+            string emailconf = Console.ReadLine();
+
+            if (emailconf.Equals(email))
+            {
+                Console.WriteLine("Email Confirmado!");
+            }
+
+            else
+            {
+                Console.WriteLine("Email incorreto.");
+            }
+                                                   
+            Console.ReadLine();
+        }
+    }
+}
